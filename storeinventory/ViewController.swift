c9c20fcc4fe569fc84e2b2d99c130fa50d1c3ee6
msbamba <msbamba2012@gmail.com>
2015-12-18 10:15:32 -0500

    First commit

---
+++ storeinventory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  storeinventory
+//
+//  Created by Mohamed Bamba on 12/16/15.
+//  Copyright © 2015 Mohamed Bamba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
